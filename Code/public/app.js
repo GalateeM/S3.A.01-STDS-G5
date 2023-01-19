4111191f4b55f9d5a99c6927d242002bdb666ed8
Caerroff <56608891+caerroff@users.noreply.github.com>
2023-01-19 10:18:06 +0100

    Update test.html.
    Co-authored-by: hugomonte <24875641+hugomonte@users.noreply.github.com>
    Co-authored-by: fiujy <momo.kerrouche31@gmail.com>

--- Code/public/app.js
+++ Code/public/app.js
@@ -1,5 +1,21 @@
 const socket = io("http://localhost:3000");
 
-socket.on("hello", (arg) => {
-    console.log(arg);
+socket.on("STDS/2/Puissance", (arg) => {
+    document.querySelector("#puissance").textContent = arg
+});
+
+socket.on("STDS/2/CO2", (arg) => {
+    document.querySelector("#co2").textContent = arg
+});
+
+socket.on("STDS/2/Température/T1", (arg) => {
+    document.querySelector("#tint").textContent = arg
+});
+
+socket.on("STDS/2/Température/T2", (arg) => {
+    document.querySelector("#text").textContent = arg
+});
+
+socket.on("STDS/2/Niveau", (arg) => {
+    document.querySelector("#quantite").textContent = arg
 });
