71aea93280ea1d38c2fc769563a5e91938ced360
Caerroff <56608891+caerroff@users.noreply.github.com>
2023-01-19 11:24:24 +0100

    Ajout de jauges + Avancé de la page Web
    Co-authored-by: hugomonte <24875641+hugomonte@users.noreply.github.com>
    Co-authored-by: fiujy <momo.kerrouche31@gmail.com>

--- Code/public/app.js
+++ Code/public/app.js
@@ -6,16 +6,27 @@ socket.on("STDS/2/Puissance", (arg) => {
 
 socket.on("STDS/2/CO2", (arg) => {
     document.querySelector("#co2").textContent = arg
+    document.querySelector("#co22").textContent = arg
 });
 
 socket.on("STDS/2/Température/T1", (arg) => {
     document.querySelector("#tint").textContent = arg
+    gaugeInt.set(document.querySelector('#tint').textContent); // set actual value
+    document.querySelector('#jaugeInt')
+    document.querySelector("#tint2").textContent = arg
 });
 
 socket.on("STDS/2/Température/T2", (arg) => {
     document.querySelector("#text").textContent = arg
+    gaugeExt.set(document.querySelector('#text').textContent); // set actual value
+    document.querySelector('#jaugeExt')
+    document.querySelector("#text2").textContent = arg
 });
 
 socket.on("STDS/2/Niveau", (arg) => {
     document.querySelector("#quantite").textContent = arg
+});
+
+socket.on("STDS/Diag", (arg) => {
+    document.querySelector("#diag").textContent = arg
 });
